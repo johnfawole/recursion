fa32407b07dfb6b4f4a709a1f254e3554f7ef928
John Fáwọlé <johnfawole18@gmail.com>
2023-06-27 01:31:51 +0100

    how to add recursive comments to apps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const commentData = {
+    title: "The Law of Contract",
+    author: "Itse Sagay",
+    comments: [
+      {
+        id: 1,
+        text: "You cannot build something on nothing and expect it to stand.",
+        author: "Niki Tobi, JSC",
+        children: [
+          {
+            id: 2,
+            text: "Commercial Law",
+            author: "Johnny Ray",
+            children: [
+              {
+                id: 3,
+                text: "Islamic Law",
+                author: "A.B. Salami",
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      { id: 4,
+        text: "Legal Method",
+        author: "The Law",
+        children: []
+        }
+    ]
+ }
 
-export default App;
+  function Comment ( {comment}) {
+      const nestedComments = (comment.children || []).map (comment => {
+        return <Comment key = {comment.id} comment = {comment} type = "child" />
+      })
+
+      return (
+        <div style = {{"marginLeft" : "25px", "marginTop" : "10px"}}>
+          <div>{comment.text}</div>
+           {nestedComments}
+        </div>
+      )
+  }
+
+  function App () {
+    return (
+      <div>
+        {
+          commentData.comments.map( (comment) => {
+            return (
+              <Comment key = {comment.id} comment = {comment} />
+            )
+          }
+          )
+        }
+      </div>
+    )
+  }
+
+   export default App;
